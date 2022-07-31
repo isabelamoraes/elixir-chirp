995ede60f6f0d7cbb245055ae623908eb75295ac
Isabela Moraes <isabelagmoraes@hotmail.com>
2022-07-30 23:29:34 -0300

    feat: update timeline in real time and list by latest post

--- lib/chirp_web/live/post_live/index.ex
+++ lib/chirp_web/live/post_live/index.ex
@@ -6,7 +6,8 @@ defmodule ChirpWeb.PostLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :posts, list_posts())}
+    if connected?(socket), do: Timeline.subscribe()
+    {:ok, assign(socket, :posts, list_posts()), temporary_assigns: [posts: []]}
   end
 
   @impl true
@@ -37,7 +38,16 @@ defmodule ChirpWeb.PostLive.Index do
     post = Timeline.get_post!(id)
     {:ok, _} = Timeline.delete_post(post)
 
-    {:noreply, assign(socket, :posts, list_posts())}
+    {:noreply, update(socket, :posts, fn posts -> [post | posts] end)}
+  end
+
+  @impl true
+  def handle_info({:post_created, post}, socket) do
+    {:noreply, update(socket, :posts, fn posts -> [post | posts] end)}
+  end
+
+  def handle_info({:post_updated, post}, socket) do
+    {:noreply, update(socket, :posts, fn posts -> [post | posts] end)}
   end
 
   defp list_posts do
